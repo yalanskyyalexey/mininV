dbc57ebe463086d27ba187da9dd7647ba8df02db
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-12-14 12:45:45 +0200

    Добавил доску для рисования, пока без эфектов

---
+++ 04-board-sources/app.js
@@ -0,0 +1,36 @@
+const board = document.querySelector('#board');
+const colors = [
+	'	#00FA9A',
+	'#7FFF00',
+	'#FF00FF',
+	'#FFA500',
+	'#32CD32',
+	'#0000CD',
+	'#4B0082',
+];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i += 1) {
+	const square = document.createElement('div');
+	square.classList.add('square');
+
+	square.addEventListener('mouseover', () => setColor(square));
+	square.addEventListener('mouseleave', () => removeColor(square));
+
+	board.append(square);
+}
+
+function setColor(element) {
+	const color = getRandomColor();
+	element.style.backgroundColor = color;
+	element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+function removeColor(element) {
+	element.style.backgroundColor = '#1d1d1d';
+	element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+	const index = Math.floor(Math.random() * colors.length);
+	return colors[index];
+}
